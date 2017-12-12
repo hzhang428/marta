2dedbcd32c56c7a1f2722b403bae3f15e3cd88fb
haozhang <hzhang428@gatech.edu>
2017-12-12 01:13:07 -0500

    Station Find by id method

--- controllers/StationController.js
+++ controllers/StationController.js
@@ -1,4 +1,5 @@
 var getConnection = require('./db.js')
+var mySQL = require('mysql')
 
 module.exports = {
     find: function(params, callback) {
@@ -7,9 +8,37 @@ module.exports = {
                 callback(err, null);
                 return;
             } else {
-                var sql = 'SELECT * FROM User';
-                con.query(sql, function(err, user) {
-                    callback(null, user);
+                var sql = 'SELECT * FROM Station';
+                con.query(sql, function(err, Stations) {
+                    callback(null, Stations);
+                    con.release();
+                });
+                return;
+            }
+        });
+    },
+
+    findById: function(params, callback) {
+        getConnection(function(err, con) {
+            if (err) {
+                callback(err, null);
+                return;
+            } else {
+                console.log(params)
+                var columns = []
+                var sql = 'SELECT ' + 
+                          'A.StopID, A.Name, A.EnterFare, A.IsTrain, A.Status, B.Intersection ' +
+                          'FROM ' + 
+                          'Station AS A ' + 
+                          'LEFT JOIN ' + 
+                          'BusStationIntersection AS B ' + 
+                          'ON ' + 
+                          'A.StopID = B.StopID ' + 
+                          'WHERE ' + 
+                          'A.StopID = ?';
+                con.query(sql, [params], function(err, Station) {
+                    console.log(Station)
+                    callback(null, Station);
                     con.release();
                 });
                 return;
--- routes/api.js
+++ routes/api.js
@@ -9,23 +9,40 @@ router.get('/:resource', function(req, res, next) {
 		StationController.find(req.query, function(err, results) {
 			if (err) {
 				res.json({
-					confirmation:"fail",
+					confirmation: "fail",
 					message: err
 				})
 				return
 			} else {
 				res.json({
-					confirmation:"success",
+					confirmation: "success",
 					message: results
 				})
 			}
 		})
-	} else {
-		res.json({
-			confirmation:"fail",
-			message: "no such resources"
+	}
+})
+
+router.get('/:resource/:stopid', function(req, res, next) {
+	var resource = req.params.resource
+	var id = req.params.stopid
+
+	if (resource == 'station') {
+		StationController.findById(id, function(err, results) {
+			if (err) {
+				res.json({
+					confirmation: "fail",
+					message: "Not Found"
+				})
+			} else {
+				res.json({
+					confirmation: "success",
+					message: results
+				})
+			}
 		})
 	}
+
 })
 
 module.exports = router
